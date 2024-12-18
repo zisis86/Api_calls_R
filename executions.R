5521a6bcd717ced8fe7f996de667406a8f038313
zisis86 <145783727+zisis86@users.noreply.github.com>
2024-12-18 17:03:57 +0200

    Update executions.R

--- executions.R
+++ executions.R
@@ -4,12 +4,10 @@ source("constant_variables.R")  # Assuming url is defined in this file
 run_bim <- function(headers, execution_info) {
   print('-- BioInfoMiner execution (please wait).. --')
   
-  response <- GET(
-    url = paste0(url, 'runbim'),
-    add_headers(.headers = headers),
-    body = execution_info,
-    encode = "json"
-  )
+  #response <- GET(url = paste0(url, 'runbim'),add_headers(.headers = headers),body = execution_info, encode = "json")
+  
+  response <- GET(paste0(url, 'runbim'), add_headers(.headers = headers), query = execution_info)
+
   
   if (status_code(response) == 200) {
     print("Successful execution!")
